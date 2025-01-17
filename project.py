b010eeabb6cfb26ed4f8b4ad2fc1e07da73d24ae
Elizabethdev1 <noahelizabeth41@gmail.com>
2025-01-17 19:51:32 +0100

    Update project.py

--- project.py
+++ project.py
@@ -110,3 +110,38 @@ button.pack(padx=10,pady=10)
 
 window.mainloop()
 
+Spanish_Dictionary = {
+    'destiny' : 'destino',
+    'bottle' : 'botella',
+    'classroom' : 'aula',
+    'food' : 'alimento',
+    'love' : 'amar',
+    'culture' : 'cultura',
+    'beautiful' : 'hermosa/hermoso',
+    'research' : 'investigacion',
+    'studio' : 'estudio',
+    'heartbreak' : 'desamor',
+    'friend' : 'amigo',
+    'house' : 'casa',
+    'cat' : 'gato',
+    'dog' : 'perro',
+    'water' : 'agua',
+    'sun' : 'sol',
+    'book' : 'libro',
+    'happy' : 'feliz',
+    'family' : 'familia',
+    'city' : 'ciudad'
+}
+
+def search1(word):
+    if word in Spanish_Dictionary:
+        result.set(Spanish_Dictionary[word])
+        print(Spanish_Dictionary[word])
+    else:
+        result.set('not found')
+        print('not found')
+
+search_btn = Button(window, text='spanish', command=lambda: search1(entry_text.get()))
+search_btn.pack(padx=10,pady=10)
+
+window.mainloop()
